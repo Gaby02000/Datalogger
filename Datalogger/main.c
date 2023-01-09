d21f7cc98d1e74493796ebafeee65894d4efd83c
Gabriel Federico Jose Gimenez Miguel <80761150+Gaby02000@users.noreply.github.com>
2023-01-09 10:33:02 -0300

    Update main.c

--- Datalogger/main.c
+++ Datalogger/main.c
@@ -1,29 +1,4 @@
 
-/**********************************************************************
- FileName:        main.c
- Dependencies:    p33FJ256GP710.h
- Processor:       dsPIC33F
- Compiler:        MPLAB® C30 v2.01 or higher
-
- Ejemplo de funcionamiento de:
- 	Timer1
- 	LCD
- 	PORT A Output
- 	PORT D Input
-
- REVISION HISTORY:
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- Author            Date      Comments on this revision
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- Richard Fischer   07/14/05  Initial Release
- Priyabrata Sinha  01/27/06  Ported to non-prototype devices 
- Ricardo López     05/24/10  Adaptación para uso general
- Sebastián Wahler  08/05/13  Nuevas adaptaciones
- Marcelo Gómez     09/03/14  Portado a MPLAB.X
- Sebastián Wahler  04/06/19  Simplificado sin Placa
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-**********************************************************************/
 #include "common.h"
 #include <xc.h>
 #include "confbits.h"
