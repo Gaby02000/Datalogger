4590c99641e37d27e27c88c90d195de52f6b986f
Gabriel Federico Jose Gimenez Miguel <80761150+Gaby02000@users.noreply.github.com>
2023-01-09 10:34:53 -0300

    Update common.h

--- Datalogger/common.h
+++ Datalogger/common.h
@@ -1,16 +1,3 @@
-/* REVISION HISTORY:
-*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-* Author            Date      Comments on this revision
-*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-* Richard Fischer   07/14/05  general variable linkage information
-* Priyabrata Sinha  01/27/06  Ported to non-prototype devices
-* Sebastián Wahler  04/06/19  Simplificado
-*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-*
-* ADDITIONAL NOTES:
-* 1. This file contains definitions commonly used in this project.
-*
-**********************************************************************/
 
 #define MAX 32
 #define TOPE 300
